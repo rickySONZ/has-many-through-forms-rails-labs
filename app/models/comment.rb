432e35a65aca3e03ee6553eae6dc8b1e4e41215d
Ryan Erricson <ryanerricson@gmail.com>
2021-04-13 13:31:02 -0400

    commit tree bdb54099f9d7d16f5b7dde3dc1dbc7c708d26654

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(ua)
+    self.user = User.find_or_create_by(ua) unless ua[:username].empty?
+  end
 end
